9b7c05565cb1c54c781d899778fe3bef9ad2591f
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-12-05 21:57:04 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,35 +16,35 @@ public class Main {
 
         else {
             if (num_1 <= num_2 && num_1 <= num_3 && num_1 <= num_4 && num_1 <= num_5) {
-                System.out.println("Min first number: " + num_1);
+                System.out.println("Min number: " + num_1);
             }
-            if (num_2 <= num_1 && num_2 <= num_3 && num_2 <= num_4 && num_2 <= num_5) {
-                System.out.println("Min second number: " + num_2);
+            else if (num_2 <= num_1 && num_2 <= num_3 && num_2 <= num_4 && num_2 <= num_5) {
+                System.out.println("Min number: " + num_2);
             }
-            if (num_3 <= num_1 && num_3 <= num_2 && num_3 <= num_4 && num_3 <= num_5) {
-                System.out.println("Min third number: " + num_3);
+            else if (num_3 <= num_1 && num_3 <= num_2 && num_3 <= num_4 && num_3 <= num_5) {
+                System.out.println("Min number: " + num_3);
             }
-            if (num_4 <= num_1 && num_4 <= num_2 && num_4 <= num_3 && num_4 <= num_5) {
-                System.out.println("Min fourth number: " + num_4);
+            else if (num_4 <= num_1 && num_4 <= num_2 && num_4 <= num_3 && num_4 <= num_5) {
+                System.out.println("Min number: " + num_4);
             }
-            if (num_5 <= num_1 && num_5 <= num_2 && num_5 <= num_3 && num_5 <= num_4) {
-                System.out.println("Min fifth number: " + num_5);
+            else if (num_5 <= num_1 && num_5 <= num_2 && num_5 <= num_3 && num_5 <= num_4) {
+                System.out.println("Min number: " + num_5);
             }
              /*---------------------------------------------------------------------*/
             if (num_1 >= num_2 && num_1 >= num_3 && num_1 >= num_4 && num_1 >= num_5) {
-                System.out.println("Max first number: " + num_1);
+                System.out.println("Max number: " + num_1);
             }
-            if (num_2 >= num_1 && num_2 >= num_3 && num_2 >= num_4 && num_2 >= num_5) {
-                System.out.println("Max second number: " + num_2);
+            else if (num_2 >= num_1 && num_2 >= num_3 && num_2 >= num_4 && num_2 >= num_5) {
+                System.out.println("Max number: " + num_2);
             }
-            if (num_3 >= num_1 && num_3 >= num_2 && num_3 >= num_4 && num_3 >= num_5) {
-                System.out.println("Max third number: " + num_3);
+            else if (num_3 >= num_1 && num_3 >= num_2 && num_3 >= num_4 && num_3 >= num_5) {
+                System.out.println("Max number: " + num_3);
             }
-            if (num_4 >= num_1 && num_4 >= num_2 && num_4 >= num_3 && num_4 >= num_5) {
-                System.out.println("Max fourth number: " + num_4);
+            else if (num_4 >= num_1 && num_4 >= num_2 && num_4 >= num_3 && num_4 >= num_5) {
+                System.out.println("Max number: " + num_4);
             }
-            if (num_5 >= num_1 && num_5 >= num_2 && num_5 >= num_3 && num_5 >= num_4) {
-                System.out.println("Max fifth number: " + num_5);
+            else if (num_5 >= num_1 && num_5 >= num_2 && num_5 >= num_3 && num_5 >= num_4) {
+                System.out.println("Max number: " + num_5);
             }
         }
     }
